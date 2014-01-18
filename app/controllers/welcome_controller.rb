0435503b70e17ce9506291dc32f0d8e8add95336
anzumana <anzumana.taal@gmail.com>
2014-01-18 18:25:43 +0000

    welcome controller and roues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,18 @@
 class WelcomeController < ApplicationController
   def index
+	
   end
+	
+	def sign_in
+		@userName = params[:userName]
+		@password = params[:password] 
+		if params[:password].to_s == '123'
+			redirect_to action: 'index'
+		end
+		if params[:password].to_s == '456'
+			redirect_to action: 'admin'
+		end
+
+		
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Test3::Application.routes.draw do
   resources :bids 
 	root :to => 'welcome#index'
 
-end
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -65,4 +64,5 @@ end
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-#   match ':controller(/:action(/:id))(.:format)'
+   match ':controller(/:action(/:id))(.:format)'
+end
